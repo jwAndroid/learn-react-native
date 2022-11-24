ec52ffbf8416e569937977f000e574fe2716c86e
jwAndroid <cjd9408er@naver.com>
2022-11-24 18:24:26 +0900

    Test re-rendering

--- PoC/toptab/App.tsx
+++ PoC/toptab/App.tsx
@@ -6,14 +6,12 @@ import {NavigationContainer} from '@react-navigation/native';
 import React, {memo} from 'react';
 
 import MainScreen from './src/MainScreen';
-import NonModule from './src/screens/NonModule';
 
 const App = () => {
   return (
-    <NonModule />
-    // <NavigationContainer>
-    //   <MainScreen />
-    // </NavigationContainer>
+    <NavigationContainer>
+      <MainScreen />
+    </NavigationContainer>
   );
 };
 
