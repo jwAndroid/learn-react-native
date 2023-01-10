7730b662572ace2f0c6ab7f4ad384e371493944a
jwAndroid <cjd9408er@naver.com>
2023-01-10 16:52:06 +0900

    poc: test-flight

---
+++ PoC/test_flight/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 30}}>Hello. ITDA! 😁</Text>
+    </View>
+  );
+};
+
+export default App;
