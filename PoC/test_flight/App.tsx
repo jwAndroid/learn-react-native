82369a746a59c77346a3e70efc63e6e49b3b5f7c
jwAndroid <cjd9408er@naver.com>
2023-01-10 17:57:29 +0900

    init

--- PoC/test_flight/App.tsx
+++ PoC/test_flight/App.tsx
@@ -2,14 +2,62 @@
  * @format
  */
 
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, SafeAreaView, View} from 'react-native';
+import axios from 'axios';
+
+import {base_url} from './apikey';
+
+type IDataType = {
+  id: number;
+  name: string;
+};
+
+const requestApi = () =>
+  axios.get<IDataType[]>(`${base_url}/api/category/first`);
 
 const App = () => {
+  const [state, setState] = useState<IDataType[]>([]);
+  const [status, setStatus] = useState(0);
+
+  const onPress = async () => {
+    try {
+      const {data, status: code} = await requestApi();
+
+      setStatus(code);
+      setState(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30}}>Hello. ITDA! 😁</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <View style={{alignItems: 'center', marginTop: 50}}>
+        <Text style={{fontSize: 30}}>Hello. ITDA! 😁</Text>
+
+        <Text style={{fontSize: 30, marginTop: 20}} onPress={onPress}>
+          START API TEST
+        </Text>
+
+        <Text style={{fontSize: 30, marginTop: 20}} onPress={onPress}>
+          {status === 200 ? 'GOOD' : 'BAD'}
+        </Text>
+      </View>
+
+      <View
+        style={{justifyContent: 'center', alignItems: 'center', marginTop: 50}}>
+        {state.map((data, index) => (
+          <Text key={`${index + 1}`}>{data.name}</Text>
+        ))}
+      </View>
+    </SafeAreaView>
   );
 };
 
