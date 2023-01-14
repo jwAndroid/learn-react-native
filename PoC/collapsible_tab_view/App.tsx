9474540667ae6d576682d3e9da427d277095bc1f
jwAndroid <cjd9408er@naver.com>
2023-01-14 23:35:26 +0900

    add new

--- PoC/collapsible_tab_view/App.tsx
+++ PoC/collapsible_tab_view/App.tsx
@@ -3,20 +3,11 @@
  */
 
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {ThemeProvider} from '@emotion/react';
 
-import {theme} from './theme';
-import Example from './src/Example';
+import Main from './example/Main';
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Example />
-      </NavigationContainer>
-    </ThemeProvider>
-  );
+  return <Main />;
 };
 
 export default App;
