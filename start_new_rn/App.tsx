d0163a540a9aaeb35d1ca709771f01a141442850
jwAndroid <cjd9408er@naver.com>
2023-08-29 23:37:16 +0900

    add: rn 0.72

---
+++ start_new_rn/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {memo} from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>2023 NEW REACT NATIVE!</Text>
+    </View>
+  );
+};
+
+export default memo(App);
