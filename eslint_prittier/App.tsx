7922295b9d5bd6d793a2a1f2db6c31764ab3c955
jwAndroid <cjd9408er@naver.com>
2023-09-13 21:53:50 +0900

    Update: Eslint

---
+++ eslint_prittier/App.tsx
@@ -0,0 +1,17 @@
+/**
+ *
+ * @format
+ */
+
+import { View, Text } from "react-native";
+import React, { memo } from "react";
+
+const App = () => {
+   return (
+      <View>
+         <Text>App</Text>
+      </View>
+   );
+};
+
+export default memo(App);
