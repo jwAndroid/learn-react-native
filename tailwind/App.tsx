aec7380f5298934c9cfb64eff61414a600e641d5
jwAndroid <cjd9408er@naver.com>
2023-09-24 20:30:35 +0900

    Update: nativewind

--- tailwind/App.tsx
+++ tailwind/App.tsx
@@ -3,14 +3,12 @@
  */
 
 import React, { memo } from 'react';
-import { View } from 'react-native';
-
-import { StyledText } from './src';
+import { Text, View } from 'react-native';
 
 const App = () => {
    return (
-      <View className='flex-1 justify-center items-center'>
-         <StyledText />
+      <View className='flex-1 justify-center items-center bg-gray-800'>
+         <Text className='font-bold italic text-cyan-300 text-xl text'>Hello Tailwind</Text>
       </View>
    );
 };
